e33fff427f29f6b5bc8293778e4de5c989c2645d
JW Nicholson <john.nicholson812@gmail.com>
2020-09-24 12:09:01 -0400

    styled landing page mobile first

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Grid, makeStyles, Paper,} from '@material-ui/core';
+import { ImCircleDown, ImCircleUp } from 'react-icons/im';
+import {Box, Grid, makeStyles, Paper, Typography,} from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
-    mainWrapper: {
+    root: {
         background:"#f8f8ff",
         color:"#141414",
         maxWidth:"900px",
         margin:"auto",
         marginTop:theme.spacing(2),
         borderRadius:"4px",
+        alignContent:"baseline",
+        justifyContent:"flex-start"
     },
+   contentWrapper:{
+    display:"flex",
+    flexFlow:"row no-wrap",
+    // justifyContent:"flex-start",
+    // alignContent:"center",
+    maxWidth:'900px',
+    padding:theme.spacing(1),
+   },
+   coinlistImage:{
+       height:"60px",
+       width:"auto",
+       marginRight:theme.spacing(2),
+   },
+   coinImageContainer:{
+        marginRight:theme.spacing(2),
+
+   },
+    currentPriceBox: {
+      color:"#141414",
+      marginRight:theme.spacing(2),
+    },
+    currentPercentage:{
+        marginRight:"0",
+    },
+    percentageArrow:{
+        marginRight:theme.spacing(1),
+    }
 }));
 
 const Coin = ({coin}) => {
@@ -20,16 +50,40 @@ const Coin = ({coin}) => {
     return (
         
             
-        <Grid container xs={12} className={classes.mainWrapper} >
-          <Link to={"/"} >
-            <Paper >
-                <img className="coinlist-image" src={coin.image} alt="coin logo" />
-            </Paper>
+        <Grid container xs={6}  className={classes.root} display="flex" flexDirection="row" >
+          <Link to={"/styx"} >
+            <Box xs={12} className={classes.contentWrapper} >
+                <Grid xs={4} item justifyContent="center" alignContent="center" className={classes.coinImageContainer}>
+                <img className={classes.coinlistImage} src={coin.image} alt="coin logo" />
+                </Grid>
+                
+                <Grid item
+                    xs={4}
+                    className={classes.currentPriceBox}
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="center"
+                >
+                    <Typography ><strong>Current Price</strong></Typography>
+                    <Typography>{coin.current_price}</Typography>
+                </Grid>
+                
+                <Grid item xs={4} >
+                    <Box 
+                        className={classes.currentPercentage}
+                        color={coin.price_change_percentage_24h < 0 ? "error.main" : "success.dark"}>
+                    {coin.price_change_percentage_24h < 0 ?   
+                <ImCircleDown className={classes.percentageArrow} /> : <ImCircleUp className={classes.percentageArrow}  />} 
+                {coin.price_change_percentage_24h}
+                 </Box>
+                </Grid>
+
+            </Box>
           </Link>
         </Grid>
            
         
-    )
+    );
 }
 
 export default Coin;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import {  Box, Typography } from '@material-ui/core';
+import {  Box, Typography, makeStyles } from '@material-ui/core';
 
+
+const useStyles = makeStyles((theme) => ({
+    header:{
+        marginTop:theme.spacing(2),
+        color:"#a67c00",
+        
+    },
+}));
 
 const Header = () => {
+    const classes = useStyles();
+
     return (
-        <header>
+        <header className={classes.header}>
             <Box 
                 display="flex"
                 justifyContent="center"   
             >
-                <a href="/">
-                    <Typography variant="h3" component="h1" gutterBottom>
-                        King Crypto
-                    </Typography>
-                </a>
+             
+            
+            <Typography  variant="h3" component="h1" gutterBottom>
+               King Crypto
+           </Typography>
+          
+                
             </Box>
         </header>
     )
