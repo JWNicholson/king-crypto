8dc1c66a42e59cfec7db6bd264d6bb661ed5d4f3
JW Nicholson <john.nicholson812@gmail.com>
2020-09-26 12:19:31 -0400

    Coin.js re-styled

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -1,98 +1,102 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ImCircleDown, ImCircleUp } from 'react-icons/im';
-import {Box, Grid, makeStyles, Button, Typography,} from '@material-ui/core';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import DeleteOutlineTwoToneIcon from '@material-ui/icons/DeleteOutlineTwoTone';
+import { Box, Card, CardContent, makeStyles, Button, Typography, } from '@material-ui/core';
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
-        background:"#f8f8ff",
-        color:"#141414",
-        maxWidth:"322px",
-        margin:"auto",
-        marginTop:theme.spacing(2),
-        borderRadius:"4px",
-        alignContent:"baseline",
-        justifyContent:"flex-start"
+        marginTop: "2rem"
     },
-   contentWrapper:{
-    display:"flex",
-    flexFlow:"row no-wrap",
-    // justifyContent:"flex-start",
-    // alignContent:"center",
-    maxWidth:'900px',
-    padding:theme.spacing(1),
-   },
-   coinlistImage:{
-       height:"60px",
-       width:"auto",
-       marginRight:theme.spacing(1),
-   },
-   coinImageContainer:{
-        marginRight:theme.spacing(1),
+    content: {
+        flex: '1 0 auto',
 
-   },
-    currentPriceBox: {
-      color:"#141414",
-      marginRight:theme.spacing(1),
     },
-    currentPercentageContainer:{
-        display:"flex",
+
+    details: {
+        display: "flex",
+        flexFlow: "row wrap",
+        alignItems: "center",
     },
-    currentPercentage:{
-        marginTop:theme.spacing(1),
-        marginRight:theme.spacing(1),
+
+    coinlistImage: {
+        maxWidth: "90px",
+        height: "auto",
     },
-    percentageArrow:{
-        
-        marginRight:theme.spacing(1),
+
+    contentBox: {
+        display: "flex",
+        flexFlow: "column nowrap",
+        alignItems: "flex-start",
+        width: "90%",
+        margin: "auto",
     },
-    deleteIconContainer:{
-       display:"flex",
-       justifyContent:"flex-end",
+    contentBox2: {
+        display: "flex",
+        flexFlow: "row nowrap",
+        color: "green",
     },
-  
-    
+    deletIconBox: {
+        color: "red",
+        display: "flex",
+    },
+
 }));
 
-const Coin = ({coin,deleteCoin}) => {
+const Coin = ({ coin, deleteCoin }) => {
     const classes = useStyles();
 
-    return (  
-        <Grid   className={classes.root}  >
-          <Link to={`/coins/${coin.id}`} >
-            <Box xs={12} md={6} className={classes.contentWrapper} >
-                <Grid xs={3} item  className={classes.coinImageContainer}>
-                <img className={classes.coinlistImage} src={coin.image} alt="coin logo" />
-                </Grid>
-                <Grid item
-                    xs={3}
-                    className={classes.currentPriceBox}  
-                >
-                    <Typography ><strong>Current Price</strong></Typography>
-                    <Typography>{coin.current_price}</Typography>
-                </Grid>
-                <Grid item xs={5} className={classes.currentPercentageContainer}>
-                    <Box 
-                        className={classes.currentPercentage}
-                        color={coin.price_change_percentage_24h < 0 ? "error.main" : "success.dark"}>
-                    {coin.price_change_percentage_24h < 0 ?   
-                <ImCircleDown className={classes.percentageArrow} /> : <ImCircleUp className={classes.percentageArrow}  />} 
-                {coin.price_change_percentage_24h}%
-                 </Box>
-                </Grid>
-                <Grid item xs={1} className={classes.deleteIconContainer}>
-                <Typography 
-                    onClick={(e) => {
-                        e.preventDefault();
-                        deleteCoin(coin.id);
-                        console.log("Delete clicked")
-                    }}
-                variant="h4" color="secondary" className={classes.deleteIcon} >&#10008;</Typography>
-                </Grid>
-            </Box>
-          </Link>
-        </Grid>
+    return (
+
+        <Card className={classes.root} variant="outlined"  >
+            <Link to={`/coins/${coin.id}`} >
+                <Box className={classes.details}>
+
+                    <CardContent className={classes.content}  >
+                        <Box className={classes.coinImgBox}>
+                            <img className={classes.coinlistImage} src={coin.image} alt="coin logo" />
+                        </Box>
+                    </CardContent>
+
+                    <CardContent className={classes.content}  >
+                        <Box className={classes.contentBox}>
+                            <Typography className={classes.contentBoxData}>Current Price</Typography>
+                            <Typography className={classes.contentBoxData}>{coin.current_price}</Typography>
+                        </Box>
+                    </CardContent>
+
+                    <CardContent className={classes.content}>
+                        <Box className={classes.contentBox2}
+
+                            color={coin.price_change_percentage_24h < 0 ? "error.main" : "success.dark"}
+
+                        >
+                            <Typography>
+                                <span>
+                                    {coin.price_change_percentage_24h < 0 ?
+                                        <ArrowDownwardIcon /> : <ArrowUpwardIcon />}
+                                </span>  {coin.price_change_percentage_24h}%</Typography>
+                        </Box>
+                    </CardContent>
+
+                    <CardContent className={classes.content}   >
+                        <Box className={classes.deletIconBox}>
+                            <DeleteOutlineTwoToneIcon fontSize="large"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    deleteCoin(coin.id);
+                                    console.log("Delete clicked")
+                                }}
+                            />
+                        </Box>
+                    </CardContent>
+
+                </Box>
+            </Link>
+        </Card>
+
     );
 }
 
--- src/components/CoinData/CoinData.js
+++ src/components/CoinData/CoinData.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { Grid,Typography } from '@material-ui/core';
+import { Box, Card,CardContent,Grid,Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme) => ({
-	root : {
-		justifyContent  : 'space-between',
-		flexGrow        : 1,
-		margin          : '28px 0',
-		padding         : '18px',
-		backgroundColor : '#f5f5f5',
-		borderRadius    : '4px'
-	}
-}));
+const useStyles = makeStyles(()=> ({
+    root:{
+        marginTop:"2rem"
+    },
+    content: {
+        flex: '1 0 auto',
+        
+      },
 
+      details: {
+          display:"flex",
+          flexFlow:"row wrap",
+          alignItems:"baseline",
+      },
+
+      coinImg:{
+          margin:"1rem",
+      },
+
+      contentBox:{
+          display:"flex",
+          flexFlow:"column nowrap",
+          alignItems:"flex-start",
+          width:"90%",
+          margin: "auto",
+      },
+      contentBox2:{
+        display:"flex",
+        flexFlow:"row nowrap",
+         color:"green",
+      },
+      deletIconBox:{
+          color:"red",
+          display:"flex", 
+      },
+
+ 
+}))
 const CoinData = ({ data }) => {
 	const classes = useStyles();
 
@@ -20,31 +47,66 @@ const CoinData = ({ data }) => {
 
 		if (data) {
 			return (
-				<Grid container className={classes.root}>
-					<Grid item xs={12} sm={4}>
-						<Typography variant="h6">Market Cap</Typography>
-						<Typography>{data.market_cap}</Typography>
-						<hr />
-						<Typography variant="h6">Volume 24HR</Typography>
-						<Typography>{data.total_volume}</Typography>
-					</Grid>
 
-					<Grid item xs={12} sm={4}>
-						<Typography variant="h6">Total Supply</Typography>
-						<Typography>{data.total_supply}</Typography>
-						<hr />
-						<Typography variant="h6">High 24HR</Typography>
-						<Typography>{data.high_24h}</Typography>
-					</Grid>
+				<Card className={classes.root} variant="outlined"  >
+				<Box className={classes.details}>
+	  
+				<CardContent className={classes.content}  >
+					<Box className={classes.coinImgBox}>
+					<Typography className={classes.coinImg}>Coin Image</Typography>
+					</Box>
+				</CardContent>
+				
+				<CardContent className={classes.content}  >
+					<Box className={classes.contentBox}>
+					<Typography className={classes.contentBoxData}>Current Price</Typography>
+					<Typography className={classes.contentBoxData}>0000000</Typography>
+					</Box>
+				</CardContent>
+	  
+				<CardContent className={classes.content}   >
+				<Box className={classes.contentBox2}>
+					<Typography><span>^</span> Up or Dwn</Typography>
+					</Box>
+				</CardContent>
+				
+	  
+				<CardContent className={classes.content}   >
+					<Box className={classes.deletIconBox}>
+					<Typography>Big x</Typography>
+					</Box>
+				</CardContent>
+				
+				</Box>
+	  
+			</Card>
 
-					<Grid item xs={12} sm={4}>
-						<Typography variant="h6">Circulating Supply</Typography>
-						<Typography>{data.circulating_supply}</Typography>
-						<hr />
-						<Typography variant="h6">Low 24HR</Typography>
-						<Typography>{data.low_24h}</Typography>
-					</Grid>
-				</Grid>
+
+				// <Grid container className={classes.root}>
+				// 	<Grid item xs={12} sm={4}>
+				// 		<Typography variant="h6">Market Cap</Typography>
+				// 		<Typography>{data.market_cap}</Typography>
+				// 		<hr />
+				// 		<Typography variant="h6">Volume 24HR</Typography>
+				// 		<Typography>{data.total_volume}</Typography>
+				// 	</Grid>
+
+				// 	<Grid item xs={12} sm={4}>
+				// 		<Typography variant="h6">Total Supply</Typography>
+				// 		<Typography>{data.total_supply}</Typography>
+				// 		<hr />
+				// 		<Typography variant="h6">High 24HR</Typography>
+				// 		<Typography>{data.high_24h}</Typography>
+				// 	</Grid>
+
+				// 	<Grid item xs={12} sm={4}>
+				// 		<Typography variant="h6">Circulating Supply</Typography>
+				// 		<Typography>{data.circulating_supply}</Typography>
+				// 		<hr />
+				// 		<Typography variant="h6">Low 24HR</Typography>
+				// 		<Typography>{data.low_24h}</Typography>
+				// 	</Grid>
+				// </Grid>
 			);
 		}
 	};
