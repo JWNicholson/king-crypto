3d155dd56dcd488cd67b966bf96a698077cfcca5
JW Nicholson <john.nicholson812@gmail.com>
2022-06-14 10:18:31 -0400

    Updated axios. Renamed coinSpecifics and coinSynopsis files.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Box, Container, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
-import CoinSynopsis from '../src/pages/CoinSynopsis';
-import CoinSpecifics from './pages/CoinSpecifics';
+import CoinSynopsis from '../src/pages/coinSynopsis';
+import CoinSpecifics from '../src/pages/coinSpecifics';
 import { CoinWatchContextProvider } from './context/coinwatchContext';
 
 const useStyles = makeStyles((theme) => ({
